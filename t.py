f8433fb15508a0b3d6cd9236b76af1f846f10f77
Malhar Deshmukh <malhar_deshmukh@outlook.com>
2018-02-23 18:44:57 +0530

    aded file for testing blob detection

--- t.py
+++ t.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 inpt = cv2.imread('0033.jpg')
 #cv2.resize(inpt,inpt, 0.5, 0.5)
 
@@ -20,13 +21,18 @@ highVal=210
 lowLimit=np.array([lowHue,lowSat,lowVal],'uint8')
 hoghLimit=np.array([highHue,highSat,highVal],'uint8')
 
-print(lowLimit,hoghLimit)
+#print(lowLimit,hoghLimit)
 
 mask=cv2.inRange(inptHSV,lowLimit,hoghLimit,)
 cv2.imshow('mask',mask)
 masked = cv2.bitwise_and(inpt,inpt,mask=mask)
 cv2.imshow("Masked",masked)
-avg=np.mean(masked,axis=0)
-print(avg)
+
+det = cv2.SimpleBlobDetector_create()
+keys = det.detect(masked)
+
+im_with_keypoints = cv2.drawKeypoints(masked, keys, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imshow('keys',im_with_keypoints)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()

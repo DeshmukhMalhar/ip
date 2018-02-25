cd80e5d79d4c359e85718bbbb9974bbe6324ba85
Malhar Desmhukh <malhar_deshmukh@outlook.com>
2018-02-25 09:41:50 +0530

    Added code for three shuttle detection

--- gpioInput.py
+++ gpioInput.py
@@ -17,6 +17,8 @@ GPIO.setup(LedPin, GPIO.OUT)
 
 ButtPin = 13
 RedShuttlePin = 3
+GreenShuttlePin = 5
+GoldShuttlePin =7
 ButtState = False
 
 def printFunction(channel):
@@ -33,6 +35,156 @@ def setup():
     GPIO.setup(ButtPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(ButtPin, GPIO.RISING, callback=printFunction, bouncetime=300)
 
+def checkForRed():
+    #from dt        
+    cam.capture('img.jpg')
+    inpt = cv2.imread('img.jpg')
+    #cv2.resize(inpt,inpt, 0.5, 0.5)
+    inpt=cv2.resize(inpt,(480,640),interpolation=cv2.INTER_AREA)
+    cv2.imshow('original',inpt)
+    blr=cv2.GaussianBlur(inpt,(7,7),0)
+    cv2.imshow('Blurred',blr)
+    inptHSV=cv2.cvtColor(blr, cv2.COLOR_BGR2HSV)
+    #[[[157 131 123]]]
+    #[[[ 77 255  55]]]
+    #[[[ 81 255 129]]]
+    #[[[ 87 142  61]]]
+    #[[[178 196 248]]]
+    lowHue=150
+    lowSat=150
+    lowVal=120
+    highHue=250
+    highSat=250
+    highVal=255
+    lowLimit=np.array([lowHue,lowSat,lowVal],'uint8')
+    hoghLimit=np.array([highHue,highSat,highVal],'uint8')
+    #print(lowLimit,hoghLimit)
+    mask=cv2.inRange(inptHSV,lowLimit,hoghLimit,)
+    cv2.imshow('mask',mask)
+    masked = cv2.bitwise_and(inpt,inpt,mask=mask)
+    cv2.imshow("Masked",masked)
+    minAvg=1.5
+    maxAvg=25
+    avg=np.average(mask)
+    print(avg)
+    if ((avg >minAvg)and (avg < maxAvg)):
+        print("Shuttle is detected")
+        GPIO.output(RedShuttlePin, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(RedShuttlePin, GPIO.LOW)
+        return 'red'
+    else:
+        print("Shuttle is not detected")
+        GPIO.output(RedShuttlePin, GPIO.LOW)
+        time.sleep(0.5)
+
+        global ButtState
+        ButtState = False
+        #cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return 'nothing'
+
+def checkForGreen():
+    #change window for green 
+    #from dt        
+    cam.capture('img.jpg')
+    inpt = cv2.imread('img.jpg')
+    #cv2.resize(inpt,inpt, 0.5, 0.5)
+    inpt=cv2.resize(inpt,(480,640),interpolation=cv2.INTER_AREA)
+    cv2.imshow('original',inpt)
+    blr=cv2.GaussianBlur(inpt,(7,7),0)
+    cv2.imshow('Blurred',blr)
+    inptHSV=cv2.cvtColor(blr, cv2.COLOR_BGR2HSV)
+    #[[[157 131 123]]]
+    #[[[ 77 255  55]]]
+    #[[[ 81 255 129]]]
+    #[[[ 87 142  61]]]
+    #[[[178 196 248]]]
+    lowHue=150
+    lowSat=150
+    lowVal=120
+    highHue=250
+    highSat=250
+    highVal=255
+    lowLimit=np.array([lowHue,lowSat,lowVal],'uint8')
+    hoghLimit=np.array([highHue,highSat,highVal],'uint8')
+    #print(lowLimit,hoghLimit)
+    mask=cv2.inRange(inptHSV,lowLimit,hoghLimit,)
+    cv2.imshow('mask',mask)
+    masked = cv2.bitwise_and(inpt,inpt,mask=mask)
+    cv2.imshow("Masked",masked)
+    minAvg=1.5
+    maxAvg=25
+    avg=np.average(mask)
+    print(avg)
+    if ((avg >minAvg)and (avg < maxAvg)):
+        print("Shuttle is detected")
+        GPIO.output(GreenShuttlePin, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(GreenShuttlePin, GPIO.LOW)
+        return 'green'
+    else:
+        print("Shuttle is not detected")
+        GPIO.output(RedShuttlePin, GPIO.LOW)
+        time.sleep(0.5)
+
+        global ButtState
+        ButtState = False
+        #cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return 'nothing'
+
+def checkForGold():
+    #change window for GOld 
+    #from dt        
+    cam.capture('img.jpg')
+    inpt = cv2.imread('img.jpg')
+    #cv2.resize(inpt,inpt, 0.5, 0.5)
+    inpt=cv2.resize(inpt,(480,640),interpolation=cv2.INTER_AREA)
+    cv2.imshow('original',inpt)
+    blr=cv2.GaussianBlur(inpt,(7,7),0)
+    cv2.imshow('Blurred',blr)
+    inptHSV=cv2.cvtColor(blr, cv2.COLOR_BGR2HSV)
+    #[[[157 131 123]]]
+    #[[[ 77 255  55]]]
+    #[[[ 81 255 129]]]
+    #[[[ 87 142  61]]]
+    #[[[178 196 248]]]
+    lowHue=150
+    lowSat=150
+    lowVal=120
+    highHue=250
+    highSat=250
+    highVal=255
+    lowLimit=np.array([lowHue,lowSat,lowVal],'uint8')
+    hoghLimit=np.array([highHue,highSat,highVal],'uint8')
+    #print(lowLimit,hoghLimit)
+    mask=cv2.inRange(inptHSV,lowLimit,hoghLimit,)
+    cv2.imshow('mask',mask)
+    masked = cv2.bitwise_and(inpt,inpt,mask=mask)
+    cv2.imshow("Masked",masked)
+    minAvg=1.5
+    maxAvg=25
+    avg=np.average(mask)
+    print(avg)
+    if ((avg >minAvg)and (avg < maxAvg)):
+        print("Shuttle is detected")
+        GPIO.output(GoldShuttlePin, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(GoldShuttlePin, GPIO.LOW)
+        return 'green'
+    else:
+        print("Shuttle is not detected")
+        GPIO.output(RedShuttlePin, GPIO.LOW)
+        time.sleep(0.5)
+
+        global ButtState
+        ButtState = False
+        #cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return 'nothing'
+
+
 def blink():
   while True:
     if ButtState is False :
@@ -41,64 +193,10 @@ def blink():
       GPIO.output(LedPin, GPIO.LOW) 
       time.sleep(1)
     else :
-        #from dt
-
-        
-        cam.capture('img.jpg')
-
-        inpt = cv2.imread('img.jpg')
-        #cv2.resize(inpt,inpt, 0.5, 0.5)
-        inpt=cv2.resize(inpt,(480,640),interpolation=cv2.INTER_AREA)
-        cv2.imshow('original',inpt)
-        blr=cv2.GaussianBlur(inpt,(7,7),0)
-        cv2.imshow('Blurred',blr)
-
-        inptHSV=cv2.cvtColor(blr, cv2.COLOR_BGR2HSV)
-
-        #[[[157 131 123]]]
-        #[[[ 77 255  55]]]
-        #[[[ 81 255 129]]]
-        #[[[ 87 142  61]]]
-        #[[[178 196 248]]]
-
-        lowHue=150
-        lowSat=150
-        lowVal=120
-
-        highHue=250
-        highSat=250
-        highVal=255
-
-        lowLimit=np.array([lowHue,lowSat,lowVal],'uint8')
-        hoghLimit=np.array([highHue,highSat,highVal],'uint8')
-
-        #print(lowLimit,hoghLimit)
-
-        mask=cv2.inRange(inptHSV,lowLimit,hoghLimit,)
-        cv2.imshow('mask',mask)
-        masked = cv2.bitwise_and(inpt,inpt,mask=mask)
-        cv2.imshow("Masked",masked)
-
-        minAvg=1.5
-        maxAvg=25
-
-        avg=np.average(mask)
-        print(avg)
-
-        if ((avg >minAvg)and (avg < maxAvg)):
-            print("Shuttle is detected")
-            GPIO.output(RedShuttlePin, GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(RedShuttlePin, GPIO.LOW)
-        else:
-            print("Shuttle is not detected")
-            GPIO.output(RedShuttlePin, GPIO.LOW)
-            time.sleep(0.5)
-
-        global ButtState
-        ButtState = False
-        #cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        checkForGold()
+        checkForGreen()
+        checkForRed()
+       
 
 
 
